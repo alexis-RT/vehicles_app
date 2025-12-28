f2b68685663e71c43b71ab6ef4b7aea0129121ae
Alan Rojas <alanrojas60@gmail.com>
2025-12-28 10:16:54 -0600

    Odometer hist button added

---
+++ app.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+cars_data = pd.read_csv('vehicles_us.csv')
+
+st.header("Car Data Analysis")
+
+car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
+hist_button = st.button('Construir histograma')  # crear un botón
+
+if hist_button:  # si se presiona el boton
+
+    st.write(
+        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+print(cars_data.head())
