fe9fef66194b125b82bf1dd6acdc050ac894b967
Alan Rojas <alanrojas60@gmail.com>
2025-12-29 02:13:23 -0600

    final version

--- app.py
+++ app.py
@@ -6,22 +6,22 @@ cars_data = pd.read_csv('vehicles_us.csv')
 
 st.header("PLATAFORMA DE VENTA DE VEHÍCULOS")
 
-car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
-hist_data = car_data.dropna(
+clean_data = cars_data.dropna(
     subset=['odometer', 'price'])  # eliminar valores nulos
 hist_button = st.checkbox('Construir histograma')  # crear un botón
-color_options = ["condition", "fuel", "transmission"]
-selected_color = st.selectbox(
-    "Selecciona la condicion del vehiculo que quieres observar:", color_options)
 
 if hist_button:  # si se presiona el boton
+
+    color_options = ["condition", "fuel", "transmission"]
+    selected_color = st.selectbox(
+        "Selecciona la variable del vehiculo que quieres observar:", color_options)
 
     st.write(
         'Histograma que muestra la distribución del kilometraje de los coches')
 
     # crear un histograma
-    fig = px.histogram(hist_data, x="odometer",
-                       y="price", color=selected_color)
+    fig = px.histogram(clean_data, x="odometer",
+                       color=selected_color, opacity=0.7)
 
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
@@ -30,18 +30,17 @@ if hist_button:  # si se presiona el boton
 # botón para gráfico de dispersión
 disp_graf_button = st.checkbox('Mostrar grafico de dispersión')
 
-color_options = ["fuel", "transmission", "condition", "type"]
-selected_color = st.selectbox("Selecciona la condicion del vehiculo que quieres observar:",
-                              color_options)
-
 if disp_graf_button:
+    color_options = ["fuel", "transmission", "condition", "type"]
+    selected_color = st.selectbox("Selecciona la variable del vehiculo que quieres observar:",
+                                  color_options)
+
     st.write(
         'Creación de un gráfico de dispersión para la relacion entre el kilometraje y el precio de los coches')
 
     # crear un gráfico de dispersión
-    fig2 = px.scatter(car_data, x="odometer", y="price", color=selected_color)
+    fig2 = px.scatter(clean_data, x="odometer",
+                      y="price", color=selected_color, opacity=0.5)
 
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig2, use_container_width=True)
-print(cars_data.columns)
-print(cars_data.head())
