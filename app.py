bea143e7e29bdff5e6a01faf972ebacfd0e35478
Alan Rojas <alanrojas60@gmail.com>
2025-12-29 01:42:54 -0600

    histogram creation

--- app.py
+++ app.py
@@ -4,20 +4,44 @@ import plotly.express as px
 
 cars_data = pd.read_csv('vehicles_us.csv')
 
-st.header("Car Data Analysis")
+st.header("PLATAFORMA DE VENTA DE VEHÍCULOS")
 
 car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
-hist_button = st.button('Construir histograma')  # crear un botón
+hist_data = car_data.dropna(
+    subset=['odometer', 'price'])  # eliminar valores nulos
+hist_button = st.checkbox('Construir histograma')  # crear un botón
+color_options = ["condition", "fuel", "transmission"]
+selected_color = st.selectbox(
+    "Selecciona la condicion del vehiculo que quieres observar:", color_options)
 
 if hist_button:  # si se presiona el boton
 
     st.write(
-        'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+        'Histograma que muestra la distribución del kilometraje de los coches')
 
     # crear un histograma
-    fig = px.histogram(car_data, x="odometer")
+    fig = px.histogram(hist_data, x="odometer",
+                       y="price", color=selected_color)
 
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
 
+# GRÁFICO DE DISPERSIÓN
+# botón para gráfico de dispersión
+disp_graf_button = st.checkbox('Mostrar grafico de dispersión')
+
+color_options = ["fuel", "transmission", "condition", "type"]
+selected_color = st.selectbox("Selecciona la condicion del vehiculo que quieres observar:",
+                              color_options)
+
+if disp_graf_button:
+    st.write(
+        'Creación de un gráfico de dispersión para la relacion entre el kilometraje y el precio de los coches')
+
+    # crear un gráfico de dispersión
+    fig2 = px.scatter(car_data, x="odometer", y="price", color=selected_color)
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig2, use_container_width=True)
+print(cars_data.columns)
 print(cars_data.head())
